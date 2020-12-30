6444703d9fe77a74307d76f30a6dfb1c4df67a73
AdiChidambaram <72016974+AdiChidambaram@users.noreply.github.com>
2020-12-30 19:24:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+var project = "My first console project";
+console.log(project);
+
+var first_name = "Aditya";
+var last_name = "Chidambaram";
+
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var number1 = 16;
+
+var number2 = 21;
+
+var add = number1+number2;
+console.log(add);
+
+var sub = number2-number1;
+console.log(sub);
+
+var mul = number1*number2;
+console.log(mul);
+
+var div = number1/number2;
+console.log(div);
